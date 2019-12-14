6c0013bad052aa701c6581fc6a5036134adf1af6
matt-clarson <clarson469@googlemail.com>
2019-12-14 12:15:59 +0000

    added base web package

---
+++ gratitude-journal-web/src/app.ts
@@ -0,0 +1,10 @@
+import { LitElement, html, customElement } from "lit-element";
+
+@customElement("gj-app")
+export class GJApp extends LitElement {
+  render() {
+    return html`
+      <p>Hello world</p>
+    `;
+  }
+}
