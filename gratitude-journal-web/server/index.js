ea55057527d30ac5189951d1b2cdd412556d4126
matt-clarson <clarson469@googlemail.com>
2020-01-15 13:45:27 +0000

    frontend server proxies graphql requests to gj-service

--- gratitude-journal-web/server/index.js
+++ gratitude-journal-web/server/index.js
@@ -1,8 +1,9 @@
 const compression = require("compression");
 const express = require("express");
+const proxy = require("express-http-proxy");
 const path = require("path");
 
-const { STATIC_DIR } = process.env;
+const { STATIC_DIR, GRAPHQL_FORWARD_TO } = process.env;
 const PORT = process.env.PORT || 80;
 
 function main() {
@@ -10,6 +11,7 @@ function main() {
   app.use(compression());
 
   app.use("/", express.static(STATIC_DIR));
+  app.all("/graphql", proxy(GRAPHQL_FORWARD_TO));
   app.get("*", (req, res) => res.sendFile(path.join(STATIC_DIR, "index.html")));
 
   app.listen(PORT);
