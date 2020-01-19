89c5cd6921d39466e791bb76f3b7308f73b14133
matt-clarson <clarson469@googlemail.com>
2020-01-19 11:50:09 +0000

    snapshots can now be updated

--- gratitude_journal_service/tests/utils.py
+++ gratitude_journal_service/tests/utils.py
@@ -1,5 +1,8 @@
+import os
 from django.test import TestCase as DjangoTestCase
 from snapshottest.unittest import TestCase as SnapshotTestCase
 
 class TestCase(DjangoTestCase, SnapshotTestCase):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.snapshot_should_update = os.environ.get('UPDATE_SNAPSHOTS') == 'true'
